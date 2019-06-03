ceb26c9a9b81c28164981e48ed45bd7851513d99
Michael Janoski <maj@mikejanoski.com>
2019-06-02 19:14:17 -0500

    Refactored functional tests to utilize helper function to check if todo is in table

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,12 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Users have heard about a cool new online to-do app. They go
 		# to check out the homepage.
@@ -32,10 +38,7 @@ class NewVisitorTest(unittest.TestCase):
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# There is still a text box inviting them to add another item. They
 		# enter "Use peacock feathers to make a fly"
@@ -45,10 +48,8 @@ class NewVisitorTest(unittest.TestCase):
 		time.sleep(1)
 
 		# The page updates again, and now shows both items on their list
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		# The user wonders whether the site will remember their list. Then they see
 		# that ther site has generated a unique URL for them -- there is some
