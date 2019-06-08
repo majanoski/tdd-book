3df29cea60ea91494c9096b93bd706167afc5811
Michael Janoski <maj@mikejanoski.com>
2019-06-08 17:20:23 -0500

    Reformatted functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -3,62 +3,65 @@ from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
+
 class NewVisitorTest(unittest.TestCase):
-	def setUp(self):
-		self.browser = webdriver.Firefox()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-	def tearDown(self):
-		self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Users have heard about a cool new online to-do app. They go
+        # to check out the homepage.
+        self.browser.get('http://localhost:8000')
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		# Users have heard about a cool new online to-do app. They go
-		# to check out the homepage.
-		self.browser.get('http://localhost:8000')
+        # They notice the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-		# They notice the page title and header mention to-do lists
-		self.assertIn('To-Do', self.browser.title)
-		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('To-Do', header_text)
+        # They are invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute(
+            'placeholder'), 'Enter a to-do item')
 
-		# They are invited to enter a to-do item straight away
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        # Someone types "Buy peacock feathers" into a text box (Their hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
-		# Someone types "Buy peacock feathers" into a text box (Their hobby
-		# is tying fly-fishing lures)
-		inputbox.send_keys('Buy peacock feathers')
+        # When they hit enter, the page updatges, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-		# When they hit enter, the page updatges, and now the page lists
-		# "1: Buy peacock feathers" as an item in a to-do list
-		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
+        # There is still a text box inviting them to add another item. They
+        # enter "Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-		# There is still a text box inviting them to add another item. They
-		# enter "Use peacock feathers to make a fly"
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Use peacock feathers to make a fly')
-		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
+        # The page updates again, and now shows both items on their list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly')
 
-		# The page updates again, and now shows both items on their list
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        # The user wonders whether the site will remember their list. Then they
+        # see that ther site has generated a unique URL for them -- there is
+        # some explanatory text to that effect.
+        self.fail('Finish the test!')
 
-		# The user wonders whether the site will remember their list. Then they see
-		# that ther site has generated a unique URL for them -- there is some
-		# explanatory text to that effect.
-		self.fail('Finish the test!')
+        # User visits that URL - their to-do list is still there.
 
-		# User visits that URL - their to-do list is still there.
+        # Satisfied, they go back to sleep
 
-		# Satisfied, they go back to sleep
 
 if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+    unittest.main(warnings='ignore')
